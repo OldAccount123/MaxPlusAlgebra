a8249e21eec3e01bee1d1d886a2b3b897a984f41
Matt Hires <mettihires@gmail.com>
2016-04-12 18:39:03 +0200

    Update AdjacencyMatrix.java

--- MaxPlusAlgebra/src/AdjacencyMatrix.java
+++ MaxPlusAlgebra/src/AdjacencyMatrix.java
@@ -1,5 +1,10 @@
-import java.util.Arrays;
 import java.util.Scanner;
+ 
+ /**
+ * The class represents the adjacency matrix of the graph.
+ * @author metti
+ *
+ */
  
 public class AdjacencyMatrix extends Matrix{
 
