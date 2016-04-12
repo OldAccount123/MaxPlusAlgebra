ee3551f741ca54dd6b95d006398f0e3a3565d098
Matt Hires <mettihires@gmail.com>
2016-04-12 18:37:54 +0200

    Create WeightMatrix.java

--- MaxPlusAlgebra/src/WeightMatrix.java
+++ MaxPlusAlgebra/src/WeightMatrix.java
@@ -1,5 +1,10 @@
 import java.util.Scanner;
 
+/**
+ * The class represents the matrix of a graph.The values are the weights each edge has.
+ * @author metti
+ *
+ */
 public class WeightMatrix extends Matrix{
 
 	public WeightMatrix(int dimension) {
