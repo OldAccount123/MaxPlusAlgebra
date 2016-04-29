ea6dea53baa6803407a1717220b2d31bdd61e179
Matej Gazda <abbysek@outlook.com>
2016-04-29 10:14:39 +0200

    Update WeightMatrix.java

--- src/WeightMatrix.java
+++ src/WeightMatrix.java
@@ -2,9 +2,7 @@
  * The class represents the matrix of a graph.The values are the weights each edge has.
  */
 public class WeightMatrix extends Matrix{
-
 	public WeightMatrix(int dimension) {
 		super(dimension);
 	}
-	
 }
